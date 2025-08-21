37e01fcce4287d03d411d65ace3cc8fc3d81cb91
Hemen Tewodros <hementewodrosb@gmail.com>
2025-08-21 20:27:26 +0300

    Deleted All the FIles

--- README.md
+++
@@ -1,1 +0,0 @@
-# promptverseV2
--- frontend/src/hooks/usePromptContract.js
+++
@@ -1,9 +0,0 @@
-import { ethers } from "ethers";
-import abi from "../contracts/PromptVoting.json";
-import contractAddress from "../contracts/contract-address.json";
-import { getSigner } from "../utils/wallet";
-
-export const getContract = () => {
-  const signer = getSigner();
-  return new ethers.Contract(contractAddress.PromptVoting, abi.abi, signer);
-};
